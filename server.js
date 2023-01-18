f765d154f65431306674b5da3d22d9163d5d55b9
noufilsheikh <noufilsheikh.ui.ux@gmail.com>
2023-01-18 18:20:18 +0530

    Build v2.2

--- server.js
+++ server.js
@@ -57,5 +57,5 @@ app.post("/create-payment-intent", async (req, res) => {
   });
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Node server listening on port ${PORT}`));
